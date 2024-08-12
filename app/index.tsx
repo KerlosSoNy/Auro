b498efd78abc2994ae45b4a24d9b45f0c9aa6de8
kero <kerlossony@yahoo.com>
2024-08-12 19:00:49 +0300

    First Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,30 @@
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native';
 
 export default function RootLayout() {
-
     return (
-        <View className="flex-1 items-center justify-center bg-primary">
-            <Text className='text-white font-pblack'>Welcome To Auro </Text>
-            <Link href={'/home' as any} style={{ color: 'red' }}>Go to home</Link>
-        </View>
+        <SafeAreaView className="bg-primary w-full h-screen">
+            <ScrollView contentContainerStyle={{ height: '100%' }}>
+                <View className="w-full justify-center items-center h-full px-4">
+                    <Image
+                        source={require('../assets/images/logo.png')}
+                        resizeMode="contain"
+                        className='w-[130px] h-[84px]' // Add width and height as required
+                    />
+                    <Image
+                        source={require('../assets/Onboarding-img.png')}
+                        resizeMode="contain"
+                        className='max-w-[380px] w-full h-[300px]' // Add width and height as required
+                    />
+                    <View className='relative mt-5 text-[40px]'>
+                        <Text className='text-white text-center font-bold text-3xl'>Discover Endless Possibilities with
+                            <Text className='text-yellow-500 font-bold text-3xl'> Aora</Text>
+                        </Text>
+                        <Image className='absolute -bottom-2  -right-0 w-[76px] h-[15px]' source={require('../assets/images/path.png')} />
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
+
 
